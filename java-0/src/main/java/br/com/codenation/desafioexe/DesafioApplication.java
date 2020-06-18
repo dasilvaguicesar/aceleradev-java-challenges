fdad3ec25bf794c9d24483ef304932d39da869fc
dasilvaguilhermecesar <dasilvaguilhermecesar@gmail.com>
2020-06-18 20:25:18 -0300

    Finished Fibonacci

--- java-0/src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ java-0/src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -1,15 +1,26 @@
 package br.com.codenation.desafioexe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DesafioApplication {
 
 	public static List<Integer> fibonacci() {
-		return null;
+
+		int finish = 15;
+		List<Integer> result = new ArrayList<>();
+
+		for(int start = 0; start<finish;start++){
+			if(start < 2){
+				result.add(start);
+			}else {
+				result.add(result.get(start-1)+result.get(start-2));
+			}
+		}
+		return result;
 	}
 
-	public static Boolean isFibonacci(Integer a) {
-		return null;
+	public static Boolean isFibonacci(Integer num) {
+		return fibonacci().contains(num);
 	}
-
 }
