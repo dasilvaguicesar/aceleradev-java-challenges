f760bf1da9ec645dd992a4cbde7880c89381a0fe
dasilvaguilhermecesar <dasilvaguilhermecesar@gmail.com>
2020-07-04 11:42:27 -0300

    jbasic structure

---
+++ java-8/src/main/java/com/challenge/CalculadorDeClasses.java
@@ -0,0 +1,23 @@
+package com.challenge;
+
+import com.challenge.interfaces.Calculavel;
+
+import java.math.BigDecimal;
+
+public class CalculadorDeClasses implements Calculavel {
+
+  @Override
+  public BigDecimal Somar() {
+    return null;
+  }
+
+  @Override
+  public BigDecimal Subtrair() {
+    return null;
+  }
+
+  @Override
+  public BigDecimal Totalizar() {
+    return null;
+  }
+}
---
+++ java-8/src/main/java/com/challenge/interfaces/Calculavel.java
@@ -0,0 +1,9 @@
+package com.challenge.interfaces;
+
+import java.math.BigDecimal;
+
+public interface Calculavel {
+  BigDecimal Somar ();
+  BigDecimal Subtrair();
+  BigDecimal Totalizar();
+}
